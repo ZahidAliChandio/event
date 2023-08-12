1910b43f0c9ffbc52820b3ed76511f6ad8969434
ZahidAliChandio <zahidalichandio240@gmail.com>
2023-08-12 15:17:58 +0500

    finalized events project without database.

--- src/app/events/[...slug]/page.tsx
+++ src/app/events/[...slug]/page.tsx
@@ -1,15 +1,63 @@
-// import { useRouter } from "next/router";
+'use client'
+
+import { Fragment } from "react";
+import { useParams } from "next/navigation";
+
+import { getFilteredEvents } from "../../../../dummy-data";
+import EventList from "../../../../components/events/event-list";
+import ResultsTitle from "../../../../components/events/results-title";
+import Button from "../../../../components/ui/button";
+import ErrorAlert from "../../../../components/ui/error-alert";
 
 function FilteredEventsPage() {
-  // const router = useRouter();
-  // const slug = router.query.slug;
+  const params = useParams();
+  
+  const { slug } = params;
+
+  if(!slug) {
+    return <p className="center">Loading...</p>
+  }
+
+  const filteredYear = +slug[0];
+  const filteredMonth = +slug[1];
+
+  if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredYear < 2021 || filteredMonth < 1 || filteredMonth > 12) {
+    return (
+      <Fragment>
+         <ErrorAlert>
+          <p className="center !mt-4 text-xl">Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    )
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth
+  });
+
+  if(!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p className="center !mt-4 text-xl">No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    )
+  }
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-      {/* <h2>{slug}</h2> */}
-      {/* <h2>{params}</h2> */}
-    </div>
+    <Fragment>
+      <ResultsTitle date={new Date(filteredYear, filteredMonth - 1)} />
+      <EventList events={filteredEvents} /> 
+    </Fragment>
   );
-}
+} 
 
 export default FilteredEventsPage;
--- src/app/events/[eventId]/page.tsx
+++ src/app/events/[eventId]/page.tsx
@@ -7,6 +7,7 @@ import { getEventById } from "../../../../dummy-data";
 import EventSummary from "../../../../components/event-detail/event-summary";
 import EventLogistics from "../../../../components/event-detail/event-logistics";
 import EventContent from "../../../../components/event-detail/event-content";
+import ErrorAlert from "../../../../components/ui/error-alert";
 
 function EventDetail () {
     const params = useParams();
@@ -14,7 +15,11 @@ function EventDetail () {
     const event = getEventById(eventId);
     
     if (!event) {
-        return <p className="text-xl m-4">No event found!</p>;
+        return (
+            <ErrorAlert>
+                <p className="text-xl m-4">No event found!</p>
+            </ErrorAlert>
+        );
     }
 
     return (
